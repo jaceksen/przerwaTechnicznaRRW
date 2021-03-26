a6fc74fa2e1b2541e4230bf14644c03b2439fde1
jacek <jaceksen@gmail.com>
2021-03-26 17:52:38 +0100

    Przerwa techniczna

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'przerwaTechniczna2';
+
+  kiedyStart = '';
+
+  constructor(
+    public http: HttpClient
+  ) {}
+
+  ngOnInit() {
+    this.http.get('../../../assets/kiedyStart', {responseType: 'text'})
+      .subscribe(data => {
+        console.log('dd', data);
+        this.kiedyStart = data
+      });
+  }
 }
